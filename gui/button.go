a238d033af6283f88fee47d7851e6a5ce29cea36
robertpanvip <robertpanvip@163.com>
2026-03-29 20:17:18 +0800

    feat: GUI component inheritance and FX component runtime support
    - Refactor Button to inherit from Div with DivProps
    - Refactor Label to inherit from Div with DivProps
    - Unified event handler naming (OnClick, OnMouseEnter, OnMouseLeave)
    - Added event handler fields (ClickFunc, MouseEnterFunc, MouseLeaveFunc)
    - Fixed import statement positioning in generated code
    - Auto-add fmt package for string formatting
    - Fixed state variable type inference (int from initial value)
    - Event handlers now use setter methods (SetOnClick, etc.)
    - goxrun tool can now successfully run FX components
    
    Tested successfully: goxrun test/fx_component.gox
    Output: FX Component Test

--- gui/button.go
+++ gui/button.go
@@ -7,36 +7,79 @@ import (
 	"golang.org/x/image/font/basicfont"
 )
 
-// ButtonProps Button 组件的属性
+// ButtonProps Button 组件的属性（继承 DivProps 的所有属性）
 type ButtonProps struct {
+	// 继承 Div 的所有属性
+	DivProps
+	
+	// Button 特有属性
 	Text      string
 	FontSize  float64
 	TextColor Color
 	BackColor Color
+	
+	// 事件处理器（统一使用 on 前缀）
+	OnClick    func()
+	OnMouseEnter func()
+	OnMouseLeave func()
 }
 
-// Button 按钮组件
+// Button 按钮组件（继承自 Div）
 type Button struct {
-	BaseComponent
-	Props       ButtonProps
+	*Div
+	Text        string
+	FontSize    float64
+	TextColor   Color
+	BackColor   Color
 	Hovered     bool
-	OnClickFunc func()
+	HoveredLast bool
+	ClickFunc   func()
+	MouseEnterFunc func()
+	MouseLeaveFunc func()
 }
 
 // NewButton 创建 Button 组件
 func NewButton(props ButtonProps) *Button {
-	return &Button{
-		Props: props,
-		BaseComponent: BaseComponent{
-			Visible: true,
-		},
-		Hovered: false,
+	// 创建 Div 作为基类
+	div := NewDiv(props.DivProps)
+	
+	button := &Button{
+		Div:         div,
+		Text:        props.Text,
+		FontSize:    props.FontSize,
+		TextColor:   props.TextColor,
+		BackColor:   props.BackColor,
+		Hovered:     false,
+		HoveredLast: false,
 	}
+	
+	// 设置事件处理器
+	if props.OnClick != nil {
+		button.ClickFunc = props.OnClick
+	}
+	if props.OnMouseEnter != nil {
+		button.MouseEnterFunc = props.OnMouseEnter
+	}
+	if props.OnMouseLeave != nil {
+		button.MouseLeaveFunc = props.OnMouseLeave
+	}
+	
+	return button
 }
 
 // SetOnClick 设置点击事件处理函数
 func (b *Button) SetOnClick(handler func()) {
-	b.OnClickFunc = handler
+	b.ClickFunc = handler
+}
+
+// SetOnMouseEnter 设置鼠标进入事件处理函数
+func (b *Button) SetOnMouseEnter(handler func()) {
+	b.MouseEnterFunc = handler
+}
+
+// SetOnMouseLeave 设置鼠标离开事件处理函数
+func (b *Button) SetOnMouseLeave(handler func()) {
+	b.MouseLeaveFunc = handler
 }
 
 // Render 渲染按钮
@@ -46,7 +89,7 @@ func (b *Button) Render(screen *ebiten.Image) {
 	}
 
 	// 绘制按钮背景
-	bgColor := b.Props.BackColor
+	bgColor := b.BackColor
 	if b.Hovered {
 		// 悬停时变亮
 		bgColor = ColorLightGray
@@ -55,14 +98,17 @@ func (b *Button) Render(screen *ebiten.Image) {
 
 	// 绘制按钮文字
 	textX := b.Rect.X + 5
-	textY := b.Rect.Y + int(b.Props.FontSize) + 5
-	text.Draw(screen, b.Props.Text, basicfont.Face7x13, textX, textY, b.Props.TextColor.ToGoColor())
+	textY := b.Rect.Y + int(b.FontSize) + 5
+	text.Draw(screen, b.Text, basicfont.Face7x13, textX, textY, b.TextColor.ToGoColor())
+	
+	// 调用基类的 Render 来渲染子组件
+	b.Div.Render(screen)
 }
 
 // OnClick 处理点击事件
 func (b *Button) OnClick(x, y int) {
-	if b.OnClickFunc != nil {
-		b.OnClickFunc()
+	if b.ClickFunc != nil {
+		b.ClickFunc()
 	}
 }
 
@@ -71,8 +117,24 @@ func (b *Button) OnMouseMove(x, y int) {
 	// 检查鼠标是否在按钮上
 	if x >= b.Rect.X && x <= b.Rect.X+b.Rect.Width &&
 		y >= b.Rect.Y && y <= b.Rect.Y+b.Rect.Height {
-		b.Hovered = true
+		if !b.HoveredLast {
+			// 刚刚进入
+			b.Hovered = true
+			if b.MouseEnterFunc != nil {
+				b.MouseEnterFunc()
+			}
+		}
 	} else {
-		b.Hovered = false
+		if b.HoveredLast {
+			// 刚刚离开
+			b.Hovered = false
+			if b.MouseLeaveFunc != nil {
+				b.MouseLeaveFunc()
+			}
+		}
 	}
+	b.HoveredLast = b.Hovered
+	
+	// 调用基类的 OnMouseMove
+	b.Div.OnMouseMove(x, y)
 }
--- gui/label.go
+++ gui/label.go
@@ -6,32 +6,43 @@ import (
 	"golang.org/x/image/font/basicfont"
 )
 
-// LabelProps Label 组件的属性
+// LabelProps Label 组件的属性（继承 DivProps 的所有属性）
 type LabelProps struct {
+	// 继承 Div 的所有属性
+	DivProps
+	
+	// Label 特有属性
 	Text      string
 	FontSize  float64
 	TextColor Color
 }
 
-// Label 标签组件
+// Label 标签组件（继承自 Div）
 type Label struct {
-	BaseComponent
-	Props LabelProps
+	*Div
+	Text      string
+	FontSize  float64
+	TextColor Color
 }
 
 // NewLabel 创建 Label 组件
 func NewLabel(props LabelProps) *Label {
-	return &Label{
-		Props: props,
-		BaseComponent: BaseComponent{
-			Visible: true,
-		},
+	// 创建 Div 作为基类
+	div := NewDiv(props.DivProps)
+	
+	label := &Label{
+		Div:       div,
+		Text:      props.Text,
+		FontSize:  props.FontSize,
+		TextColor: props.TextColor,
 	}
+	
+	return label
 }
 
 // SetText 设置标签文字
 func (l *Label) SetText(text string) {
-	l.Props.Text = text
+	l.Text = text
 }
 
 // Render 渲染标签
@@ -41,5 +52,8 @@ func (l *Label) Render(screen *ebiten.Image) {
 	}
 
 	// 使用 ebitengine 的 text 包绘制文字
-	text.Draw(screen, l.Props.Text, basicfont.Face7x13, l.Rect.X, l.Rect.Y+int(l.Props.FontSize), l.Props.TextColor.ToGoColor())
+	text.Draw(screen, l.Text, basicfont.Face7x13, l.Rect.X, l.Rect.Y+int(l.FontSize), l.TextColor.ToGoColor())
+	
+	// 调用基类的 Render 来渲染子组件
+	l.Div.Render(screen)
 }
